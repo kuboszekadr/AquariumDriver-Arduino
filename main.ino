430ccc0ce54f7fd2afb1032d6005230349b6bc90
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-01-22 21:19:15 +0100

    Serial communication added for testing purposes

--- main.ino
+++ main.ino
@@ -5,14 +5,21 @@
 
 void setup() 
 {
+    Serial.begin(9600);
+    Serial.println("Starting");
     i2c::setup();  // join I2C bus
+    strcpy(i2c::response, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim odio in orci lobortis tincidunt. Ut tempor magna a sollicitudin euismod. Sed hendrerit pellentesque nisl, quis iaculis mi blandit vel. Vivamus sed porta tellus.");
+    Serial.println("Setup finished");
 }
 
-void loop() 
+void loop()
 {
+    
     if (i2c::transmissionStep == i2c::FINISHED)
     {
         executeOrder();
+        i2c::clearBuffer();
+        i2c::transmissionStep = i2c::EMPTY;
     }
     else if (i2c::transmissionStep == i2c::EMPTY)
     {
@@ -27,18 +34,22 @@ void scanSensors()
 
 void executeOrder()
 {
+    Serial.print("New order received - ");
     switch (i2c::order)
     {
     case i2c::UPDATE_RTC:
-        // UPDATE RTC
+        Serial.println("RTC udate");
         break;
     case i2c::UPDATE_WIFI_STATUS:
-        // UPDATE WIFI STATUS
+        Serial.println("WiFI status change");
         break;
     case i2c::WATER_CHANGE:
-        // START WATER CHANGE
+        Serial.println("Water change");
         break;
     default:
+        Serial.println("unknown command");
+        Serial.println("Command message:");
+        Serial.println(i2c::command);
         break;
     }
 
