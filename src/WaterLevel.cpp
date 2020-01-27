6e8382e2930d3b3afb5299f1cbddcbe38200c80e
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-01-27 20:08:34 +0100

    Initial commit

---
+++ src/WaterLevel.cpp
@@ -0,0 +1,47 @@
+#include "WaterLevel.h"
+
+WaterLevel::WaterLevel(int echo, int trig, int id_sensor)
+{
+	_echo = echo; // echo pin
+	_trig = trig; // trig pin
+	_id_sensor = id_sensor;
+
+	pinMode(_trig, OUTPUT);
+	pinMode(_echo, INPUT);
+}
+
+WaterStatus WaterLevel::getWaterStatus()
+{
+	if (_last_reading_value < WATER_LEVEL_UPPER_LEVEL) // too much water
+		return OVERFILLED;
+	else if (_last_reading_value > WATER_LEVEL_LOWER_LEVEL) // water is lacking
+		return LACKING;
+	else
+		return OK;
+}
+
+bool WaterLevel::makeReading()
+{
+	//  check if sensor is ready or if data is full
+	if (!isReady() || isAvailable())
+	{
+		return false;
+	}
+
+	// returns water level in cm
+	digitalWrite(_trig, LOW);
+	delayMicroseconds(2);
+
+	digitalWrite(_trig, HIGH);
+	delayMicroseconds(10);
+
+	// Measuring distance
+	digitalWrite(_trig, LOW);
+
+	// count how many readings are available in the array
+	_readings_count = (_readings_count + 1) % SENSOR_SAMPLING_AMOUNT; // for safety reasons
+	_readings[_readings_count] = pulseIn(_echo, HIGH) / 58.0;  // insert new reading data
+
+	_last_reading = millis();
+	return true;
+}
