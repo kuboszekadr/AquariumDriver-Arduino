9b1a692a3356c7dbaac0257a6379ca4bf4f8831b
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-02-05 21:08:14 +0100

    Cosmetic changes in tabulation

--- src/I2CSlave.h
+++ src/I2CSlave.h
@@ -8,35 +8,35 @@
 
 namespace i2c
 {
-    enum Order
-    {
-        UNKNOWN = -1,
-        UPDATE_RTC,
-        UPDATE_WIFI_STATUS,  
-        WATER_CHANGE
-    };
+enum Order
+{
+    UNKNOWN = -1,
+    UPDATE_RTC,
+    UPDATE_WIFI_STATUS,
+    WATER_CHANGE
+};
 
-    enum TransmissionStep
-    {
-        EMPTY,  // to be set separatelly when slave is done with parsing the command
-        ONGOING,  // transmission is in progress
-        FINISHED  // all data received from the master
-    };
+enum TransmissionStep
+{
+    EMPTY,   // to be set separatelly when slave is done with parsing the command
+    ONGOING, // transmission is in progress
+    FINISHED // all data received from the master
+};
 
-    extern char dataBuffer[512];  // for storing data from sensors
-    extern char commandBuffer[128];  // for storing commands from the master
-    // extern char responseBuffer[128];  // for storing response message
-    
-    extern TransmissionStep transmissionStep;  // current transmission step
-    extern Order order;  // task to be perfomed
+extern char dataBuffer[512];    // for storing data from sensors
+extern char commandBuffer[128]; // for storing commands from the master
+// extern char responseBuffer[128];  // for storing response message
 
-    void begin(int address);  // initalize I2C
-    void receiveEvent(int count);
-    void requestEvent();
+extern TransmissionStep transmissionStep; // current transmission step
+extern Order order;                       // task to be perfomed
 
-    void clearBuffer();
+void begin(int address); // initalize I2C
+void receiveEvent(int count);
+void requestEvent();
 
-    Order parseOrder();
-}
+void clearBuffer();
+
+Order parseOrder();
+} // namespace i2c
 
 #endif
