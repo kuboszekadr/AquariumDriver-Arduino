8e7bf5ec18b324f11d4495bb4d6ad9b0ccf8f570
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-05-24 05:49:51 +0200

    Added support for multi-paged OLED display

--- main.ino
+++ main.ino
@@ -29,7 +29,10 @@
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
 #include <SoftwareSerial.h>
+
+#include <MemoryFree.h>
 /*---------------------*/
 
 // Prototypes
@@ -51,24 +54,24 @@ RTC &rtc = RTC::init(RTC_RTS, RTC_CLK, RTC_DAT);
 //SD
 #define SD_PIN 53
 
-// DS18B20 - Thermometer
-#define THERMOMETER_PIN 2
-#define THERMOMETER_SENSOR_ID 1
-#define THERMOMETER_MEASURE_ID 1
-#define THERMOMETER_TEMP_LOW 24.8
-#define THERMOMETER_TEMP_HIGH 25.2
-uint8_t thermometer_address[8] = {0x28, 0x25, 0x34, 0xE5, 0x8, 0x0, 0x0, 0x35};
-Measures t[1] = {Measures::TEMP};
+// // DS18B20 - Thermometer
+// #define THERMOMETER_PIN 2
+// #define THERMOMETER_SENSOR_ID 1
+// #define THERMOMETER_MEASURE_ID 1
+// #define THERMOMETER_TEMP_LOW 24.8
+// #define THERMOMETER_TEMP_HIGH 25.2
+// uint8_t thermometer_address[8] = {0x28, 0x25, 0x34, 0xE5, 0x8, 0x0, 0x0, 0x35};
+// Measures t[1] = {Measures::TEMP};
 
-Thermometer thermometer(THERMOMETER_PIN, thermometer_address, THERMOMETER_SENSOR_ID, t,
-                        "WaterTemp",
-                        (float)THERMOMETER_TEMP_LOW, (float)THERMOMETER_TEMP_HIGH,
-                        Events::EventType::TEMP_LOW, Events::EventType::TEMP_HIGH);
+// Thermometer thermometer(THERMOMETER_PIN, thermometer_address, THERMOMETER_SENSOR_ID, t,
+//                         "WaterTemp",
+//                         (float)THERMOMETER_TEMP_LOW, (float)THERMOMETER_TEMP_HIGH,
+//                         Events::EventType::TEMP_LOW, Events::EventType::TEMP_HIGH);
 
-// // Heater
-// #define HEATER_PIN 3
-// Events::EventType heater_programs[2] = {Events::EventType::TEMP_LOW, Events::EventType::TEMP_HIGH};
-// Programs::Program heater = Programs::Program(HEATER_PIN, heater_programs, 2);
+// Heater
+#define HEATER_PIN 3
+Events::EventType heater_programs[2] = {Events::EventType::TEMP_LOW, Events::EventType::TEMP_HIGH};
+Programs::Program heater = Programs::Program(HEATER_PIN, heater_programs, 2);
 
 // // WATER LEVEL SENSOR - HC-SR04
 #define WATER_LEVEL_SENSOR_ECHO_PIN 3
@@ -117,10 +120,20 @@ PhSensor ph_sensor(PH_SENSOR_PIN, PH_SENSOR_SENSOR_ID, p,
 #define DHT_COVER_SENSOR_ID 4
 Measures d[2] = {Measures::TEMP, Measures::HUMIDITY};
 
-DHT22 dht_cover(DHT_COVER_PIN, DHT_COVER_SENSOR_ID, d,
-                "DHTCover",
+DHT22 dht_cover_left(DHT_COVER_PIN, DHT_COVER_SENSOR_ID, d,
+                "CoverLeft",
                 0.0, 0.0,
                 Events::EventType::EMPTY, Events::EventType::EMPTY);
+
+DHT22 dht_cover_center(DHT_COVER_PIN, DHT_COVER_SENSOR_ID + 1, d,
+                 "CoverCenter",
+                 0.0, 0.0,
+                 Events::EventType::EMPTY, Events::EventType::EMPTY);
+
+DHT22 dht_cover_right(DHT_COVER_PIN, DHT_COVER_SENSOR_ID + 2, d,
+                 "CoverRight",
+                 0.0, 0.0,
+                 Events::EventType::EMPTY, Events::EventType::EMPTY);
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -129,15 +142,20 @@ DHT22 dht_cover(DHT_COVER_PIN, DHT_COVER_SENSOR_ID, d,
 #define OLED_DC 10
 #define OLED_CS 11
 #define OLED_RESET 9
+
 Display display = Display::getInstance();
+Timestamp ts;
+float i2c_buffer_size;
 
 void setup()
 {
-    RTC::setTimestamp("20200516 060000");
+    Serial.begin(9600);
+
     Logger::setSD(SD_PIN);
     Logger::log(F("Starting"), LogLevel::VERBOSE);
 
-    display.begin(OLED_DC, OLED_RESET, OLED_CS);
+    display.begin(OLED_DC, OLED_RESET, OLED_CS, &ts);
+    initDisplayRows();
 
     water_change_task.schedule(10, 30);
     scheduler.addTask(&water_change_task);
@@ -168,6 +186,7 @@ void loop()
         i2c::clearBuffer();
         i2c::order = i2c::NONE;
         i2c::transmission_step = i2c::EMPTY;
+        Serial.println(freeMemory());
     }
     // do not scan sensors when I2C transmission is in progress
     else if (i2c::transmission_step == i2c::EMPTY)
@@ -178,6 +197,9 @@ void loop()
         Events::notifySubscribers();
         scheduler.loop(); // check for scheduled program runs
     }
+
+    RTC::now(ts);
+    display.show();
 }
 
 void scanSensors()
@@ -210,10 +232,9 @@ void scanSensors()
 
             // generate JSON and add to data buffer to be send to the database
             reading.toJSON(reading_json);
-            // i2c::addToBuffer(reading_json);
+            i2c::addToBuffer(reading_json);
 
             Logger::log(reading_json, LogLevel::DATA);
-            displayReading(reading);
 
             // // check if some event has to be rised
             // event = Events::EventType::EMPTY; //sensor->checkTriggers();
@@ -223,11 +244,13 @@ void scanSensors()
             //     Logger::log(msg, LogLevel::EVENT);
             // }
 
-            // sprintf(msg, "Buffer size %d", strlen(i2c::data_buffer));
-            // display.printOther(msg);
+            i2c_buffer_size =  strlen(i2c::data_buffer);
 
             memset(reading_json, 0, 150);
             memset(timestamp, 0, 20);
+
+            RTC::now(ts);
+            display.show();
         }
     }
 }
@@ -236,7 +259,7 @@ void scanSensors()
 void changeWater()
 {
     Logger::log(F("Starting scheduled water change"), LogLevel::APPLICATION);
-    water_change.changeWater();
+    // water_change.changeWater();
 }
 
 void executeOrder()
@@ -252,20 +275,30 @@ void executeOrder()
     }
 }
 
-void displayReading(Reading &r)
+void initDisplayRows()
 {
-    switch (r.id_sensor)
-    {
-    case 1:
-        // display.printTemp(r.value);
-        break;
-    case 2:
-        // display.printWaterLevel(r.value);
-        break;
-    case 3:
-        // display.printPh(r.value);
-        break;
-    default:
-        break;
-    }
+    // Page 1
+    display.initRow("Temp", (dht_cover_left.getReadings() + 1)); // to change to temp
+    display.initRow("Ph", ph_sensor.getReadings());
+    display.initRow("Water level", water_level_sensor.getReadings());
+    display.initRow("Buffer size", &i2c_buffer_size);
+
+    // Page 2
+    display.initRow("Cover-left", nullptr);
+    display.initRow("Temp", dht_cover_left.getReadings());
+    display.initRow("Humidity", (dht_cover_left.getReadings())+1);
+    display.initRow("", nullptr);
+
+    // Page 3
+    display.initRow("Cover-center", nullptr);
+    display.initRow("Temp", dht_cover_center.getReadings());
+    display.initRow("Humidity", (dht_cover_center.getReadings())+1);
+    display.initRow("", nullptr);
+
+    // Page 4
+    display.initRow("Cover-right", nullptr);
+    display.initRow("Temp", dht_cover_right.getReadings());
+    display.initRow("Humidity", (dht_cover_right.getReadings())+1);
+    display.initRow("", nullptr);
+
 }
