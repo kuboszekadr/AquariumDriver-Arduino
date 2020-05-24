fea143a06dd1fc39eae7220fc583856490dfcc8a
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-05-24 05:48:28 +0200

    Finished declaration of format function; new enum DateFormat

--- src/Timestamp.h
+++ src/Timestamp.h
@@ -4,13 +4,20 @@
 #include <Arduino.h>
 #include <RtcDateTime.h>
 
-struct Timestamp
+enum DateFormat
 {
-    Timestamp() {};
+    STANDARD, // YYYYMMDD
+    JSON,     // YYYYMMDD hhmmss
+    OLED   // YYYY-MM-DD hh:mm:ss
+}; 
+
+typedef struct Timestamp
+{
+    Timestamp(){};
     Timestamp(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute, uint8_t second);
     Timestamp(const RtcDateTime &dt);
 
-    void format(uint8_t format, char *target);
+    void format(DateFormat format, char *target);
     void truncate(uint8_t level);
 
     unsigned long getDate();
@@ -27,7 +34,6 @@ struct Timestamp
     uint8_t hour = 0;
     uint8_t minute = 0;
     uint8_t second = 0;
-}
-;
+};
 
 #endif
