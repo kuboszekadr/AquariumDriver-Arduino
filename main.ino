fb3d63690c59063a1003d294a1b37e7d357156c6
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-05-28 21:25:52 +0200

    Script clean up

--- main.ino
+++ main.ino
@@ -1,31 +1,9 @@
-/*---------------------*/
-#include "src/I2CSlave.h"
-#include "src/Events.h"
+/*------ LOCAL LIBRARIES ------*/
+#include "src/Pins.h"  // pins definisions
+#include "src/Globals.h" // global variables initalisation
+#include "src/Config.h"
 
-#include "src/DHT22.h"
-#include "src/Display.h"
-
-#include "src/Log.h"
-
-#include "src/Programs.h"
-#include "src/PhSensor.h"
-
-#include "src/Reading.h"
-#include "src/RTC.h"
-
-#include "src/Sensor.h"
-
-#include "src/Task.h"
-#include "src/TaskScheduler.h"
-#include "src/Thermometer.h"
-#include "src/Timestamp.h"
-
-#include "src/WaterChange.h"
-#include "src/WaterLevel.h"
-
-#include "src/Utils.h"
-
-/*---------------------*/
+/*------ GLOBAL LIBRARIES ------*/
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -33,31 +11,31 @@
 #include <SoftwareSerial.h>
 
 #include <MemoryFree.h>
-/*---------------------*/
 
-// Prototypes
-void changeWater();
+/*------ GLOBAL VARIABLES ------*/
+Display display = Display::getInstance();
+RTC &rtc = RTC::init(RTC_RTS_PIN, RTC_CLK_PIN, RTC_DAT_PIN);
 
-/*---------------------*/
+// Heater
+Events::EventType heater_programs[2] = {Events::EventType::TEMP_LOW, Events::EventType::TEMP_HIGH};
+Programs::Program heater = Programs::Program(HEATER_RELAY_PIN, heater_programs, 2);
 
-// I2C
-#define I2C_ADDRESS 8
+Programs::WaterChange water_change = Programs::WaterChange(WATER_LEVEL_RELAY_POMP_PIN, WATER_LEVEL_RELAY_WATER_PIN);
+TaskScheduler::Scheduler &scheduler = TaskScheduler::Scheduler::getInstance();
 
-/*---------------------*/
-// RTC
-#define RTC_RTS 6
-#define RTC_CLK 8
-#define RTC_DAT 7
-RTC &rtc = RTC::init(RTC_RTS, RTC_CLK, RTC_DAT);
+/*------ SENSORS IDs -----------*/
+/*------ (by ID)   -----------*/
+#define THERMOMETER_SENSOR_ID 1
+#define WATER_LEVEL_SENSOR_ID 2
+#define PH_SENSOR_SENSOR_ID 3
 
-/*---------------------*/
-//SD
-#define SD_PIN 53
+#define DHT_COVER_LEFT_SENSOR_ID 4
+#define DHT_COVER_CENTER_SENSOR_ID 5
+#define DHT_COVER_RIGHT_SENSOR_ID 6
 
-// // DS18B20 - Thermometer
-// #define THERMOMETER_PIN 2
-// #define THERMOMETER_SENSOR_ID 1
-// #define THERMOMETER_MEASURE_ID 1
+/*------ SENSOR INIT ------------*/
+/*------ (by its' ID) ---------------*/
+// DS18B20 - Thermometer
 // #define THERMOMETER_TEMP_LOW 24.8
 // #define THERMOMETER_TEMP_HIGH 25.2
 // uint8_t thermometer_address[8] = {0x28, 0x25, 0x34, 0xE5, 0x8, 0x0, 0x0, 0x35};
@@ -68,93 +46,65 @@ RTC &rtc = RTC::init(RTC_RTS, RTC_CLK, RTC_DAT);
 //                         (float)THERMOMETER_TEMP_LOW, (float)THERMOMETER_TEMP_HIGH,
 //                         Events::EventType::TEMP_LOW, Events::EventType::TEMP_HIGH);
 
-// Heater
-#define HEATER_PIN 3
-Events::EventType heater_programs[2] = {Events::EventType::TEMP_LOW, Events::EventType::TEMP_HIGH};
-Programs::Program heater = Programs::Program(HEATER_PIN, heater_programs, 2);
-
-// // WATER LEVEL SENSOR - HC-SR04
-#define WATER_LEVEL_SENSOR_ECHO_PIN 3
-#define WATER_LEVEL_SENSOR_TRIG_PIN 4
-
-#define WATER_LEVEL_POMP_PIN 1
-#define WATER_LEVEL_WATER_PIN 2
-
+// WATER LEVEL SENSOR - HC-SR04
 #define WATER_LEVEL_LOW 15.0
 #define WATER_LEVEL_HIGH 10.0
 
-#define WATER_LEVEL_SENSOR_ID 2
 #define WATER_LEVEL_MEASURE_ID 2
-Measures w[1] = {Measures::WATER_LEVEL};
+Measures water_level_measure[1] = {Measures::WATER_LEVEL};
 
-WaterLevel water_level_sensor(WATER_LEVEL_SENSOR_ECHO_PIN, WATER_LEVEL_SENSOR_TRIG_PIN, WATER_LEVEL_SENSOR_ID, w,
+WaterLevel water_level_sensor(WATER_LEVEL_SENSOR_ECHO_PIN, WATER_LEVEL_SENSOR_TRIG_PIN, WATER_LEVEL_SENSOR_ID, water_level_measure,
                               "WaterLevelSump",
                               (float)WATER_LEVEL_LOW, (float)WATER_LEVEL_HIGH,
                               Events::EventType::WATER_LOW, Events::EventType::WATER_HIGH);
 
-// Water change
-Programs::WaterChange water_change = Programs::WaterChange(WATER_LEVEL_POMP_PIN, WATER_LEVEL_WATER_PIN);
-
-// Scheduler & Tasks
-TaskScheduler::Scheduler &scheduler = TaskScheduler::Scheduler::getInstance();
-TaskScheduler::Task water_change_task = TaskScheduler::Task("WaterChange", changeWater);
-
 // Ph sensor
-#define PH_SENSOR_PIN 12
-
-#define PH_SENSOR_CO2_PIN 30
 #define PH_SENSOR_PH_LOW 6.5
 #define PH_SENSOR_PH_HIGH 6.7
 
-#define PH_SENSOR_SENSOR_ID 3
 #define PH_SENSOR_MEASURE_ID 3
-Measures p[1] = {Measures::PH};
+Measures ph_measure[1] = {Measures::PH};
 
-PhSensor ph_sensor(PH_SENSOR_PIN, PH_SENSOR_SENSOR_ID, p,
+PhSensor ph_sensor(PH_SENSOR_PIN, PH_SENSOR_SENSOR_ID, ph_measure,
                    "PhSensor",
                    (float)PH_SENSOR_PH_LOW, (float)PH_SENSOR_PH_HIGH,
                    Events::EventType::PH_LOW, Events::EventType::PH_HIGH);
 
-// DHT
-#define DHT_COVER_PIN 12
-#define DHT_COVER_SENSOR_ID 4
-Measures d[2] = {Measures::TEMP, Measures::HUMIDITY};
 
-DHT22 dht_cover_left(DHT_COVER_PIN, DHT_COVER_SENSOR_ID, d,
-                "CoverLeft",
-                0.0, 0.0,
-                Events::EventType::EMPTY, Events::EventType::EMPTY);
+// DHT's
+Measures dht_measures[2] = {Measures::TEMP, Measures::HUMIDITY};
 
-DHT22 dht_cover_center(DHT_COVER_PIN, DHT_COVER_SENSOR_ID + 1, d,
-                 "CoverCenter",
-                 0.0, 0.0,
-                 Events::EventType::EMPTY, Events::EventType::EMPTY);
+DHT22 dht_cover_left(DHT_COVER_LEFT_PIN, DHT_COVER_LEFT_SENSOR_ID, dht_measures,
+                     "CoverLeft",
+                     0.0, 0.0,
+                     Events::EventType::EMPTY, Events::EventType::EMPTY);
 
-DHT22 dht_cover_right(DHT_COVER_PIN, DHT_COVER_SENSOR_ID + 2, d,
-                 "CoverRight",
-                 0.0, 0.0,
-                 Events::EventType::EMPTY, Events::EventType::EMPTY);
+DHT22 dht_cover_center(DHT_COVER_CENTER_PIN, DHT_COVER_CENTER_SENSOR_ID, dht_measures,
+                       "CoverCenter",
+                       0.0, 0.0,
+                       Events::EventType::EMPTY, Events::EventType::EMPTY);
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+DHT22 dht_cover_right(DHT_COVER_RIGHT_PIN, DHT_COVER_RIGHT_SENSOR_ID, dht_measures,
+                      "CoverRight",
+                      0.0, 0.0,
+                      Events::EventType::EMPTY, Events::EventType::EMPTY);
 
-// Declaration for SSD1306 display connected using software SPI (default case):
-#define OLED_DC 10
-#define OLED_CS 11
-#define OLED_RESET 9
+/*---------------------*/
+void changeWater();
+TaskScheduler::Task water_change_task = TaskScheduler::Task("WaterChange", changeWater);
 
-Display display = Display::getInstance();
 Timestamp ts;
 float i2c_buffer_size;
 
 void setup()
 {
-    Serial.begin(9600);
-
-    Logger::setSD(SD_PIN);
+    SD.begin(SD_PIN);
     Logger::log(F("Starting"), LogLevel::VERBOSE);
 
-    display.begin(OLED_DC, OLED_RESET, OLED_CS, &ts);
+    Config::saveSensorConfig();
+    Config::loadSensorConfig();
+
+    display.begin(OLED_DC_PIN, OLED_RESET_PIN, OLED_CS_PIN, &ts);
     initDisplayRows();
 
     water_change_task.schedule(10, 30);
@@ -244,13 +194,14 @@ void scanSensors()
             //     Logger::log(msg, LogLevel::EVENT);
             // }
 
-            i2c_buffer_size =  strlen(i2c::data_buffer);
+            i2c_buffer_size = strlen(i2c::data_buffer);
 
             memset(reading_json, 0, 150);
             memset(timestamp, 0, 20);
 
             RTC::now(ts);
             display.show();
+            Serial.println(freeMemory());
         }
     }
 }
@@ -259,7 +210,7 @@ void scanSensors()
 void changeWater()
 {
     Logger::log(F("Starting scheduled water change"), LogLevel::APPLICATION);
-    // water_change.changeWater();
+    water_change.changeWater();
 }
 
 void executeOrder()
@@ -286,19 +237,18 @@ void initDisplayRows()
     // Page 2
     display.initRow("Cover-left", nullptr);
     display.initRow("Temp", dht_cover_left.getReadings());
-    display.initRow("Humidity", (dht_cover_left.getReadings())+1);
+    display.initRow("Humidity", (dht_cover_left.getReadings()) + 1);
     display.initRow("", nullptr);
 
     // Page 3
     display.initRow("Cover-center", nullptr);
     display.initRow("Temp", dht_cover_center.getReadings());
-    display.initRow("Humidity", (dht_cover_center.getReadings())+1);
+    display.initRow("Humidity", (dht_cover_center.getReadings()) + 1);
     display.initRow("", nullptr);
 
     // Page 4
     display.initRow("Cover-right", nullptr);
     display.initRow("Temp", dht_cover_right.getReadings());
-    display.initRow("Humidity", (dht_cover_right.getReadings())+1);
+    display.initRow("Humidity", (dht_cover_right.getReadings()) + 1);
     display.initRow("", nullptr);
-
 }
