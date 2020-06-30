0a3f31f6b287e0da36e688d7917f5ccc395b3d3c
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-06-30 22:12:09 +0200

    Moved task name into progmem, removed bug in isExecutable yielding wrong comparision, unified timestamp passing with Lighting library

--- src/TaskScheduler.h
+++ src/TaskScheduler.h
@@ -1,13 +1,14 @@
 #ifndef TaskScheduler_h
 #define TaskScheduler_h
 
+#include "I2CSlave.h"
 #include "Task.h"
 #include "Log.h"
 
 #include <Arduino.h>
 
 #define TASK_SCHEDULER_MAX_TASKS 6
-#define TASK_SCHEDULER_SCAN_INTERVAL 500L  // scan tasks once a minute
+#define TASK_SCHEDULER_SCAN_INTERVAL 10000L  // scan tasks once a minute
 
 namespace TaskScheduler
 {
@@ -21,8 +22,8 @@ public:
 private:
     Scheduler(){};
     Task *_tasks[TASK_SCHEDULER_MAX_TASKS];
-    unsigned int _tasks_amount = 0;
-    unsigned long _last_scan = 0L;
+    uint8_t _tasks_amount = 0;
+    uint32_t _last_scan = 0L;
 };
 } // namespace TaskScheduler
 
