a494f037d14b9a9c39c0a86fe2d3f90d71d6b362
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-06-30 22:15:42 +0200

    New function added - addToI2CBuffer as json support

--- src/Programs.cpp
+++ src/Programs.cpp
@@ -1,8 +1,9 @@
 #include "Programs.h"
 
-Programs::Program::Program(int relay_pin, const Events::EventType *events_subscription, int events)
+Programs::Program::Program(uint8_t relay_pin, uint8_t id, const Events::EventType *events_subscription, uint8_t events)
 {
     _relay = new Relay(relay_pin);
+    _id = id;
 
     Events::EventType *event = events_subscription;
     for (int i = 0; i < events; i++)
@@ -18,11 +19,13 @@ void Programs::Program::start()
         return;
     }
 
+    addToI2CBuffer(1);
     _relay->turnOn();
 }
 
 void Programs::Program::end()
 {
+    addToI2CBuffer(0, 0);
     _relay->turnOff();
 }
 
@@ -39,4 +42,16 @@ void Programs::Program::reactForEvent(Events::EventType event)
     }
 
     // TODO
+}
+
+void Programs::Program::addToI2CBuffer(uint8_t active, uint8_t step=1)
+{
+    char timestamp[17] = {};
+    RTC::getTimestamp(timestamp);
+
+    char json[100] = {};
+    sprintf_P(json, PSTR("{\"pid\":%d,\"a\":%d,\"s\":%d,\"t\":%s}"), _id, active, step, timestamp);
+    
+    i2c::addToBuffer(json);
+    Serial.println(json);
 }
