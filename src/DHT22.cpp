2346ad00edb56db818f9ea2a69c21149df587b07
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-07-25 14:17:18 +0200

    Prepared library for productiosation - DHT22 inherits from DHT class; changes related to inheritance

--- src/DHT22.cpp
+++ src/DHT22.cpp
@@ -4,9 +4,10 @@ Sensor::DHT22::DHT22(uint8_t pin, uint8_t id_sensor, Measures *id_measures,
 					 const char *name,
 					 float trigger_value_low, float trigger_value_high,
 					 Events::EventType trigger_low, Events::EventType trigger_high)
-	: Sensor(id_sensor, id_measures, 2, name, trigger_value_low, trigger_value_high, trigger_low, trigger_high)
+	: Sensor(id_sensor, id_measures, 2, name, trigger_value_low, trigger_value_high, trigger_low, trigger_high), DHT(pin, DHT_22)
 {
 	_pin = pin;
+	begin();
 }
 
 bool Sensor::DHT22::makeReading()
@@ -15,8 +16,18 @@ bool Sensor::DHT22::makeReading()
 	if (!isReady() || isAvailable())
 		return false;
 
-	_readings[0] += 70.00;
-	_readings[1] += 22.20;
+	// float humid = readHumidity();
+	// float temp = readTemperature();
+
+	// if (isnan(humid) || isnan(temp))
+	// {
+	// 	return false;
+	// }
+	
+	// _readings[0] += humid;
+	// _readings[1] += temp;
+	_readings[0] += 70.0;
+	_readings[1] += 35.0;
 	_readings_count++;
 
 	_last_reading = millis();
